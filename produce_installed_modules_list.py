fb2aaf881429fb52a982dcc25f5ec0ba6adbfbef
Jose Munoz <MunozCriolloJJ@cardiff.ac.uk>
2021-05-05 14:30:34 +0100

    fixed some parsing bugs and changed the funtion to list the directory contents.

--- produce_installed_modules_list.py
+++ produce_installed_modules_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import pandas as pd 
 import argparse
 import os.path
@@ -12,7 +12,7 @@ plt.switch_backend('agg')
 parser = argparse.ArgumentParser(description='Create a list of installed modules with descriptions')
 parser.add_argument("-t","--theme", dest='theme', metavar='THEME',
                     required=False,help='select application theme to produce list',
-                    choices=["all","chemistry","genomics"],
+                    choices=["all","biology","compilers","chemistry","creative","environment","genomics","languages","libraries","machine-learning","materials","medical","physics","tools"],
                     default="all")
 args = parser.parse_args()
 
@@ -32,13 +32,30 @@ else:
 # function to list the content of a directory, returns a list with the output of
 # bash command 'ls -1 directory', so it would work better if passed an absolute
 # path
-def list_directory_content(directory):
-    bashCommand="ls -1 " + directory
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE,
-            universal_newlines=True)
-    output, error = process.communicate()
-    return(output)
+def list_directory_content(directory,level):
+    content_files=[]
+    content_dirs=[]
+    for root, dirs, files in walklevel(directory, level):
+        files = [f for f in files if not f[0] == '.']
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+        for name in files:
+            content_files.append(os.path.join(root, name))
+        for name in dirs:
+            content_dirs.append(name)
+    return(content_files,content_dirs)
 
+def walklevel(some_dir, level=1):
+    some_dir = some_dir.rstrip(os.path.sep)
+    assert os.path.isdir(some_dir)
+    num_sep = some_dir.count(os.path.sep)
+    for root, dirs, files in os.walk(some_dir,topdown=True):
+        yield root, dirs, files
+        num_sep_this = root.count(os.path.sep)
+        if num_sep + level <= num_sep_this:
+            del dirs[:]
+
+# create a figure to represent the distribution of 
+# installed software among different fields
 def pie_chart(number_of_apps_list):
     # search for zeros and mark the indices
     zeros_indexes=[i for i, e in enumerate(number_of_apps_list) if e == 0]
@@ -67,7 +84,7 @@ def pie_chart(number_of_apps_list):
             horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
             connectionstyle = "angle,angleA=0,angleB={}".format(ang)
             kw["arrowprops"].update({"connectionstyle": connectionstyle})
-            ax.annotate('{0}%'.format(100*round(sizes[i],3)), xy=(x, y), 
+            ax.annotate('{0:.2f}%'.format(100*round(sizes[i],3)), xy=(x, y), 
                     xytext=(1.35*np.sign(x), 1.4*y),
                     horizontalalignment=horizontalalignment, **kw)
     
@@ -75,10 +92,26 @@ def pie_chart(number_of_apps_list):
             fontdict={'fontsize':14})
     plt.savefig('pie_chart.svg')
 
-#def func(pct, allvals):
-#    absolute = int(pct/100.*np.sum(allvals))
-#    return "{:.1f}%\n({:d} g)".format(pct, absolute)
-
+# Open module file, extract help text and clean it a bit
+# we assume that 'module_file' exists and is a file
+def get_help_text(module_file):
+    f = open(module_file,'r')
+    content = f.read()
+    help_text=re.findall('^set help_text(.*)',content,re.M)
+    if len(help_text)>0:
+        # remove  '     "\\t'
+        help_text_string=re.sub('.*"\\\\t','',help_text[0])
+        # remove closing '"'
+        help_text_string=re.sub('"$','',help_text_string)
+        # remove '\n'
+        help_text_string=re.sub('\\\\n',' ',help_text_string)
+        # remove '\t'
+        help_text_string=re.sub('\\\\t',' ',help_text_string)
+        # try creating a hyperlink in markdown format
+        help_text_string=re.sub('See:? (https?:.+?)( |$).*',r"[See their website for more details.](\1)",help_text_string)
+    else:
+        help_text_string='missing description'
+    return(help_text_string)
 
 # The main loop that searches for modules files in the application directories.
 # It iterates through the fields defined in 'themes'
@@ -89,41 +122,32 @@ for t in themes:
     root='/apps/modules/'+t
     if t=="compilers":
         root='/apps/modules/'+t+'/compiler'
-    root_directory_content=list_directory_content(root)
-    number_of_apps=len(root_directory_content.splitlines());
-    number_of_apps_list.append(number_of_apps)
+    dummy,apps=list_directory_content(root,0)
+    apps.sort()
+    #number_of_apps=len(apps);
+    #number_of_apps=len(apps.splitlines());
+    number_of_apps_list.append(len(apps))
 
     print('## {0}'.format(t))
     print('|Name|Versions available|Description|')
     print('|----|------------------|-----------|')
-    #print('{0}\t{1}'.format(t,number_of_apps))
-    for app in root_directory_content.splitlines():
-        versions=list_directory_content(root+'/'+app)
-        #print(app)
-        #print(versions.splitlines())
+    for app in apps:
+        module_files,dummy=list_directory_content(root+'/'+app,1)
+        versions=[]
+        for mf in module_files:
+            versions.append(mf.replace(root+'/'+app+'/',''))
+
         # Instead of looping through all versions for the same app and parse their
         # help text, we only do it for the first one. This assumes that the help text
         # is similar (same?) across versions.
-        #for v in versions.splitlines():
-        if len(versions.splitlines()):
-            module_file=root+"/"+app+"/"+versions.splitlines()[0]
+        if len(module_files):
+            module_file=module_files[0]
+            
             if os.path.isfile(module_file):
-                f = open(module_file,'r')
-                content = f.read()
-                help_text=re.findall('^set help_text(.*)',content,re.M)
-                if len(help_text)>0:
-                    help_text_string=re.sub('.*"\\\\t','',help_text[0])
-                    
-                    print('|{0}|{1}|{2}|'.format(app,
-                        ' ,'.join(versions.splitlines()),help_text_string))
-                    #print '\t{0}\t{1}\t{2}'.format(app,help_text_string,
-                    #        ' ,'.join(versions.splitlines()))
-                else:
-                    print('|{0}|{1}|missing description|'.format(app,
-                        ' ,'.join(versions.splitlines()),help_text_string))
-                    #print '\t{0}\t{1}'.format(app,"missing description",
-                    #        ' ,'.join(versions.splitlines()))
+                help_text_string=get_help_text(module_file)
+                print('|{0}|{1}|{2}|'.format(app,
+                    ', '.join(versions),help_text_string))
             else:
-                print '{0}\t{1}'.format(module_file,"not a file")
-
+                print('{0}\t{1}'.format(module_file,"not a file"))
+            
 pie_chart(number_of_apps_list)
